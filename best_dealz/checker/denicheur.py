c7f47a29368239b9d49ba4e8e0db74ad8ce4b724
NyxAether <contact.nyxhemera@gmail.com>
2024-04-01 03:43:41 +0200

    changed error message

--- best_dealz/checker/denicheur.py
+++ best_dealz/checker/denicheur.py
@@ -39,6 +39,8 @@ class Denicheur(PriceChecker):
             }
         )
         r = session.get(uri, timeout=10)
+        if r.status_code != 200:
+            raise ValueError(f"Error while getting products: {r.text}")
         soup = BeautifulSoup(r.text, "html.parser")
         articles_html = soup.find_all(attrs={"data-test": "ProductGridCard"})
         articles: list[Article] = []
